bb85c78a3df18ad3d84eefaa219509bcb0f5aff7
Paranjay Singh <parnexcodes@gmail.com>
2022-01-28 11:10:51 +0530

    remove some junk

--- components/SeasonCards.js
+++ components/SeasonCards.js
@@ -8,8 +8,8 @@ const SeasonCards = (props) => {
             {if (tvDetail.seasons.length == 1) {
                 {if (season.poster_path == null){
                     return (
-                        <div key={index} className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
-                        <div key={index} className="rounded-md overflow-hidden">
+                        <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
+                        <div className="rounded-md overflow-hidden">
                             <Link key={index} href="/tv/[id]/season/[seasonid]" as={`/tv/${tvDetail.id}/season/${index+1}`}>
                             <a title={season.name}>
                             <img className="w-full hover:opacity-70" title={season.name} src={`https://i.imgur.com/wjVuAGb.png`} alt={season.name} width="1500px" height="2250px"/>
@@ -21,8 +21,8 @@ const SeasonCards = (props) => {
                     )                    
                 }}
                 return (
-                    <div key={index} className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
-                    <div key={index} className="rounded-md overflow-hidden">
+                    <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
+                    <div className="rounded-md overflow-hidden">
                         <Link key={index} href="/tv/[id]/season/[seasonid]" as={`/tv/${tvDetail.id}/season/${index+1}`}>
                         <a title={season.name}>
                         <img className="w-full hover:opacity-70" title={season.name} src={`https://image.tmdb.org/t/p/w500${season.poster_path}`} alt={season.name} width="1500px" height="2250px"/>
@@ -36,8 +36,8 @@ const SeasonCards = (props) => {
             else {
                 {if (season.poster_path == null) {
                     return (
-                        <div key={index} className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
-                        <div key={index} className="rounded-md overflow-hidden">
+                        <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
+                        <div className="rounded-md overflow-hidden">
                             <Link key={index} href="/tv/[id]/season/[seasonid]" as={`/tv/${tvDetail.id}/season/${index}`}>
                             <a title={season.name}>
                             <img className="w-full hover:opacity-70" title={season.name} src={`https://i.imgur.com/wjVuAGb.png`} alt={season.name} width="1500px" height="2250px"/>
@@ -49,8 +49,8 @@ const SeasonCards = (props) => {
                     )                    
                 }}
                 return (
-                    <div key={index} className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
-                    <div key={index} className="rounded-md overflow-hidden">
+                    <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
+                    <div className="rounded-md overflow-hidden">
                         <Link key={index} href="/tv/[id]/season/[seasonid]" as={`/tv/${tvDetail.id}/season/${index}`}>
                         <a title={season.name}>
                         <img className="w-full hover:opacity-70" title={season.name} src={`https://image.tmdb.org/t/p/w500${season.poster_path}`} alt={season.name} width="1500px" height="2250px"/>
