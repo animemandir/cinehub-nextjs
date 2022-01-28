20326060118691d7dcb9384e323084d2eb035680
Paranjay Singh <parnexcodes@gmail.com>
2022-01-28 11:09:22 +0530

    add placeholder image (too much junk code lol)

--- components/SeasonInfo.js
+++ components/SeasonInfo.js
@@ -19,16 +19,30 @@ const SeasonInfo = (props) => {
           </div> */}
           <div className="flex flex-wrap flex-row justify-center h-full">
           {epArray.map((element, index) => {
-              return (
-                  <div key={index} className="px-2 py-2 overflow-hidden">
-                  <Link href="/tv/[id]/season/[seasonid]/[epid]" as={`/tv/${tvID}/season/${SeasonDetail.season_number}/${index+1}`}>
-                  <a key={index}>
-                  <img key={index} className="w-72 rounded-sm hover:opacity-70" src={`https://image.tmdb.org/t/p/w500${element.still_path}`} alt={element.name} />
-                  <p key={index} className="bg-zinc-800 max-w-[18rem] rounded-sm truncate text-gray-300 text-sm leading-0 font-semibold py-2">{SeasonDetail.season_number} x {index+1} : {element.name}</p>
-                  </a>
-                  </Link>
-                  </div>
-              )
+              {if (element.still_path == null) {
+                return (
+                    <div key={index+1} className="px-2 py-2 overflow-hidden">
+                    <Link href="/tv/[id]/season/[seasonid]/[epid]" as={`/tv/${tvID}/season/${SeasonDetail.season_number}/${index+1}`}>
+                    <a key={index+2}>
+                    <img key={index+3} className="w-72 rounded-sm hover:opacity-70" src={`https://i.imgur.com/HIYYPtZ.png`} alt={element.name} />
+                    <p key={index+4} className="bg-zinc-800 max-w-[18rem] rounded-sm truncate text-gray-300 text-sm leading-0 font-semibold py-2">{SeasonDetail.season_number} x {index+1} : {element.name}</p>
+                    </a>
+                    </Link>
+                    </div>
+                )
+              }
+            else {
+                return (
+                    <div key={index+1} className="px-2 py-2 overflow-hidden">
+                    <Link href="/tv/[id]/season/[seasonid]/[epid]" as={`/tv/${tvID}/season/${SeasonDetail.season_number}/${index+1}`}>
+                    <a key={index+2}>
+                    <img key={index+3} className="w-72 rounded-sm hover:opacity-70" src={`https://image.tmdb.org/t/p/w500${element.still_path}`} alt={element.name} />
+                    <p key={index+4} className="bg-zinc-800 max-w-[18rem] rounded-sm truncate text-gray-300 text-sm leading-0 font-semibold py-2">{SeasonDetail.season_number} x {index+1} : {element.name}</p>
+                    </a>
+                    </Link>
+                    </div>
+                ) 
+            }}
           })}
           </div>
           </div>
