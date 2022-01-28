5907e1cae4847a00173769106274b3706bd1a9c4
Paranjay Singh <parnexcodes@gmail.com>
2022-01-28 09:31:50 +0530

    add bg on text

--- components/SeasonInfo.js
+++ components/SeasonInfo.js
@@ -23,8 +23,8 @@ const SeasonInfo = (props) => {
               return (
                   <div key={index} className="px-2 py-2 overflow-hidden">
                   <a key={index} href={`/tv/${tvID}/season/${SeasonDetail.season_number}/${index+1}`}>
-                  <img key={index} className="w-72 rounded-lg hover:opacity-70" src={`https://image.tmdb.org/t/p/w500${element.still_path}`} alt={element.name} />
-                  <p key={index} className="max-w-[17rem] truncate text-gray-300 justify-center text-sm leading-0 font-semibold py-2">{SeasonDetail.season_number} x {index+1} : {element.name}</p>
+                  <img key={index} className="w-72 rounded-sm hover:opacity-70" src={`https://image.tmdb.org/t/p/w500${element.still_path}`} alt={element.name} />
+                  <p key={index} className="bg-zinc-800 max-w-[18rem] rounded-sm truncate text-gray-300 text-sm leading-0 font-semibold py-2">{SeasonDetail.season_number} x {index+1} : {element.name}</p>
                   </a>
                   </div>
               )
