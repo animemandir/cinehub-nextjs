7298b1ceb49a784aa2dcfe3490371b0df2bf179b
parnexcodes <parnexcodes@gmail.com>
2022-04-15 15:25:27 +0530

    Update index.js

--- pages/search/index.js
+++ pages/search/index.js
@@ -45,8 +45,8 @@ useEffect(() => {
                             <div className="my-2 px-2 w-full overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/5 xl:my-2 xl:px-2 xl:w-1/5">
                             <div className="rounded-md overflow-hidden">
                                 <Link key={MovieCard.id} href="/movie/[id]" as={`/movie/${MovieCard.id}`}>
-                                <a title={MovieCard.title}>
-                                <img className="w-11/12 hover:opacity-70" title={MovieCard.title} src={`https://image.tmdb.org/t/p/w500${MovieCard.poster_path}`} alt={MovieCard.title} width="1500px" height="2250px"/>
+                                <a title={MovieCard.title} key={MovieCard.id}>
+                                <img className="w-11/12 hover:opacity-70" key={MovieCard.id} title={MovieCard.title} src={`https://image.tmdb.org/t/p/w500${MovieCard.poster_path}`} alt={MovieCard.title} width="1500px" height="2250px"/>
                                 </a>
                                 </Link>
                             </div>
